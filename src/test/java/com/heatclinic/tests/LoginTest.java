68a93009e5680a4ef5e8606c04dbb5d25292a555
Taniya Aktar <you@example.com>
2021-11-07 12:25:12 -0500

    added framework

--- src/test/java/com/heatclinic/tests/LoginTest.java
+++ src/test/java/com/heatclinic/tests/LoginTest.java
@@ -1,2 +1,14 @@
-package com.heatclinic.tests;public class LoginTest {
+package com.heatclinic.tests;
+
+import com.heatclinic.pages.LoginPage;
+import com.heatclinic.pages.RegistrationPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BasePageTest {
+    @Test
+    public void LoginTest() throws InterruptedException {
+        System.out.println("This is a login Page");
+        loginPage.login();
+    }
 }
--- src/test/java/com/heatclinic/tests/RegistrationTest.java
+++ src/test/java/com/heatclinic/tests/RegistrationTest.java
@@ -1,2 +1,13 @@
-package com.heatclinic.tests;public class RegistrationTest {
+package com.heatclinic.tests;
+
+import com.heatclinic.pages.BasePage;
+import org.testng.annotations.Test;
+
+public class RegistrationTest extends BasePageTest {
+
+    @Test
+    public void registrationTest() throws InterruptedException {
+        System.out.println("This is Heat Clinic");
+        registrationPage.registerAccount();
+    }
 }
